263cb754e3fb6cb45406410643d0d3cedc5823fc
4myNicole <91946031+4myNicole@users.noreply.github.com>
2022-03-10 09:56:42 +0000

    Add files via upload

---
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,54 @@
+function getReceipt() { //add all clicked options at the bottom
+    var text1 = "<h3>You Ordered:</h3>";
+    var runningTotal = 0;
+    var sizeTotal = 0;
+    var sizeArray = document.getElementsByClassName("size");
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1+selectedSize+"<br>";
+        }
+    }
+
+    if (selectedSize === "Mouse sized") { //giving the sizes a price
+        sizeTotal = 8;
+    } else if (selectedSize === "Dog sized") {
+        sizeTotal = 12;
+    } else if (selectedSize === "Horse sized") {
+        sizeTotal = 16;
+    } else if (selectedSize === "Elephant sized") {
+        sizeTotal = 20;
+    } else if (selectedSize === "Dinosaur sized") {
+        sizeTotal = 26;
+    }
+    runningTotal = sizeTotal; 
+    console.log(selectedSize+" = £"+sizeTotal+".00");
+    console.log("size text1: "+text1);
+    console.log("subtotal: £"+runningTotal+".00");
+    getTopping(runningTotal,text1);
+};
+
+function getTopping(runningTotal,text1) {
+    var toppingTotal = 0;
+    var selectedTopping = [];
+    var toppingArray = document.getElementsByClassName("toppings");
+    for (var j = 0; j < toppingArray.length; j++) {
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value+")");
+            text1 = text1+toppingArray[j].value+"<br>";
+        }
+    }
+    var toppingCount = selectedTopping.length;
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount -1);
+    } else {
+        toppingTotal = 0;
+    }
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("total selected topping items: "+toppingCount);
+    console.log(toppingCount+" topping - 1 free topping = "+"£"+toppingTotal+".00");
+    console.log("topping text1: "+text1);
+    console.log("Purchase Total: "+"£"+runningTotal+".00");
+    document.getElementById("showText").innerHTML=text1;
+    document.getElementById("totalPrice").innerHTML= "</h3>Total: <strong>£"+runningTotal+".00"+"</strong></h3>";
+};
